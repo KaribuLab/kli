127cf348240003b14597219c2c9614c9d14b121b
Patricio Ascencio <patricio.ascencio@karibu.cl>
2026-02-16 15:56:27 -0300

    fix(project): Redirect command output to stdout and stderr in runHook function

--- project/project_posthooks.go
+++ project/project_posthooks.go
@@ -1,6 +1,7 @@
 package project
 
 import (
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -9,6 +10,8 @@ func runHook(workdir string, cmd string) error {
 	tokens := strings.Split(cmd, " ")
 	command := exec.Command(tokens[0], tokens[1:]...)
 	command.Dir = workdir
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
 	err := command.Run()
 	if err != nil {
 		return err
